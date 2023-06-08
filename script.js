2bec4125171152e791a4187a944d298b62b1a7eb
Gkamashidze <gqamashidze51@gmail.com>
2023-06-08 22:59:10 +0400

    განახლება 1.0

---
+++ script.js
@@ -0,0 +1,21 @@
+function wrisgadatana() {
+    document.getElementById("mainID").style.display = "flex";
+
+    document.getElementById("mainID").style.justifyContent = "flex-end";
+
+
+}
+
+// function changecolor() {
+
+//     document.getElementById("mainDiv").style.backgroundColor = "green";
+
+// }
+
+if (mainID.style.justifyContent == "flex-end") {
+    document.getElementById("mainDiv").style.backgroundColor = "green";
+}
+
+
+
+
